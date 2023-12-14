d1c7676fd6e52e009b9688e84218f81406c1809a
wanghao <wanghao@ninghao.net>
2023-12-14 12:30:52 +0800

    Jenkinsfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ import git from 'gulp-git';
 
 gulp.task('watch', () => {
   // 监视文件变化
-  gulp.watch(['Jenkinsfile', 'jenkins/**/*'], (cb) => {
+  gulp.watch(['Jenkinsfile', 'jenkins/**/*'], (event, cb) => {
     // 执行 Git 提交并推送
+    console.log('File(s) changed:', event.path);
+
     gulp
       .src('.')
       .pipe(git.add())
-      .pipe(git.commit('Jenkinsfile updated'))
+      .pipe(git.commit('自动提交'))
       .on('end', function () {
         git.push('origin', 'main', () => {
           cb();
--- jenkins/demo.ts
+++ jenkins/demo.ts
@@ -1,1 +1,1 @@
-console.log('hello');
+console.log('hello～');
