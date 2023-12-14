264dd81f39c84a5912aa32cee7c13401dd8ba30d
wanghao <wanghao@ninghao.net>
2023-12-14 12:25:58 +0800

    Jenkinsfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,16 @@ import gulp from 'gulp';
 import git from 'gulp-git';
 import watch from 'gulp-watch';
 
-gulp.task('watch', function () {
-  // 监视 Jenkinsfile 文件的变化
-  gulp.watch('Jenkinsfile', function (cb) {
+gulp.task('watch', () => {
+  // 监视文件变化
+  gulp.watch('Jenkinsfile', (cb) => {
     // 执行 Git 提交并推送
     gulp
       .src('.')
       .pipe(git.add())
       .pipe(git.commit('Jenkinsfile updated'))
       .on('end', function () {
-        git.push('origin', 'main', function (err) {
-          if (err) {
-            return cb(err);
-          }
+        git.push('origin', 'main', () => {
           cb();
         });
       });
